60fd90099777fdf05213d118c6a549e5dc85c80a
Berra <esma.berra.karaman@hotmail.com>
2025-08-27 19:21:13 +0300

    add data and guidelines

---
+++ guidelines.js
@@ -0,0 +1,11 @@
+//data.js dosyasindaki students array'ini kullanarak asagidaki gorevleri tamamla
+
+// 1. 10. siradaki ogrencinin ismini logla
+// 2. Students array'ine yeni bir ogrenci ekle
+// 3. Ogrencileri attendance'a gore sirala ve studentsByAttendance degiskenine ata
+// 4. Ogrencilerden aktif olmayanlari listele
+// 5. Yeni bir ogrenci ekleyen addStudent fonksiyonunu yaz
+// 6. Ogrencilerin not ortalamasini hesaplayan calculateAverage fonksiyonunu yaz
+// 7. Ogrencilerin not ortalamasini ogrenci bilgilerine ekleyen setAverages fonksiyonunu yaz
+// 8. En yuksek ortalamaya sahip olan ogrenciyi logla
+// 10. js101 dersinin kac kisi tarafindan alindigini bul
